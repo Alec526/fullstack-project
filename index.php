928029b216b31951a4a0c6b5471d4a0d2da39b90
Alec <aarnaoutoglou@roc-dev.com>
2022-02-15 10:08:53 +0100

    added navigation bar

--- index.php
+++ index.php
@@ -8,8 +8,30 @@
     <title>Website</title>
 </head>
 <body>
-    <div>
-        <p>Welcome</p>
-    </div>
+
+<div class="header">
+    Welcome
+</div>
+
+<div id="mySidenav" class="sidenav">
+    <li><a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a></li>
+    <li><a href="index.php">Home</a></li>
+    <li><a href="about.php">About</a></li>
+    <li><a href="news.php">News</a></li>
+    <li><a href="flufftext.php">flufftext</a></li>
+    <li><a href="contact.php">Contact</a></li>
+</div>
+
+<span style="font-size:30px;cursor:pointer" onclick="openNav()">&#9776;</span>
+
+<script>
+    function openNav() {
+        document.getElementById("mySidenav").style.width = "250px";
+    }
+
+    function closeNav() {
+        document.getElementById("mySidenav").style.width = "0";
+    }
+</script>
 </body>
 </html>
