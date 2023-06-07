9af141dc40cbe2f943a75015a31169c34a5008e0
Alec <aarnaoutoglou@roc-dev.com>
2023-06-07 10:19:45 +0200

    setting up basic database connection

--- database.php
+++ database.php
@@ -1,1 +1,14 @@
 <?php
+
+$servername = "localhost";
+$dbname = "casus_cafe";
+$username = "root";
+$password = "pass";
+
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=casus_cafe", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully";
+} catch (PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
