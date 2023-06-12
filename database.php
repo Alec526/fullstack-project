771c15bddb6ad8af49cac43c1bd4d93a86cd8bbc
Alec <aarnaoutoglou@roc-dev.com>
2023-06-12 09:49:02 +0200

    connection established

--- database.php
+++ database.php
@@ -3,7 +3,7 @@
 $servername = "localhost";
 $dbname = "casus_cafe";
 $username = "root";
-$password = "pass";
+$password = "";
 
 try {
     $conn = new PDO("mysql:host=$servername;dbname=casus_cafe", $username, $password);
@@ -13,8 +13,8 @@ try {
     echo "Connection failed: " . $e->getMessage();
 }
 
-$query = "SELECT * from gast";
-$stmt = $conn->prepare($query) or die ("Error 1.");
-$stmt->execute() or die ("Error 2.");
-$row = $stmt->fetch() or die ("Error 3.");
-echo $row["idgast"];
+//$query = "SELECT * from gast";
+//$stmt = $conn->prepare($query) or die ("Error 1.");
+//$stmt->execute() or die ("Error 2.");
+//$row = $stmt->fetch() or die ("Error 3.");
+//echo $row["idgast"];
